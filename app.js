c54ea83d43851cdf9117f106ba41a4f0dbcc73e5
Ivan Hell <hell.ivan@yahoo.com>
2015-07-06 18:00:54 +0200

    Basic functionality is working

--- app.js
+++ app.js
@@ -1,72 +1,75 @@
-var app = angular.module('documee_demo', ['ui.bootstrap', 'ngFacebook']);
+var app = angular.module('documee_demo', ['ui.bootstrap']);
 
 
 
-app.config(
-    function( $facebookProvider) {
-        $facebookProvider.setAppId("1136006799750257");
-        $facebookProvider.setPermissions([
-            "public_profile",
-            "email",
-            "user_friends",
-            "read_custom_friendlists",
-            "user_likes",
-            "user_about_me",
-            "user_birthday",
-            "user_location",
-            "user_posts"
-        ]);
-    }
-);
 
-app.controller("mainController", function ($http, $scope, $facebook) {
+app.controller("mainController", function ($http, $scope, $rootScope) {
 
 
     OAuth.initialize("U7oog1cN5o_ZsjeoQ_rPOxbFaKA");
 
 
-    $scope.fb_access_token = undefined;
-    $scope.twitter_access_token = undefined;
+    $scope.auth = {};
 
+    $scope.auth.fb ={
+        authenticated : false,
+        credentials : {}
+    };
 
-    $scope.isFacebookLoggedIn = false;
-    $scope.isTwitterLoggedIn = false;
+    $scope.auth.twitter ={
+        authenticated : false,
+        credentials : {}
+    };
 
-
-    function set_fb_credentials(accessToken){
-        $scope.fb_access_token = accessToken;
-        $http.defaults.headers.common.fb_access_token = accessToken;
+    function set_fb_credentials(access_token){
+        $scope.auth.fb.authenticated = true;
+        $scope.auth.fb.credentials.access_token = access_token;
+        $http.defaults.headers.common.fb_access_token = access_token;
     }
+
+    function set_twitter_credentials(oauth_token, oauth_token_secret){
+        $scope.auth.twitter.authenticated = true;
+        $scope.auth.twitter.credentials.oauth_token = oauth_token;
+        $scope.auth.twitter.credentials.oauth_token_secret = oauth_token_secret;
+        $http.defaults.headers.common.twitter_oauth_token = oauth_token;
+        $http.defaults.headers.common.twitter_oauth_token_secret = oauth_token_secret;
+    }
+
+
 
     $scope.loginTwitter = function(){
         OAuth.popup('twitter').done(function(result) {
             console.log("Authenticated with twitter");
             console.log(result);
-        })
+            set_twitter_credentials(result.oauth_token, result.oauth_token_secret);
+            $rootScope.$apply();
+        });
     };
 
     $scope.loginFacebook = function(){
         OAuth.popup('facebook').done(function(result) {
             console.log("Authenticated with facebook");
             console.log(result);
-        })
-    };
-
-    $scope.login = function(){
-        $facebook.login().then(function(response) {
-            refresh();
+            set_fb_credentials(result.access_token);
+            $rootScope.$apply();
         });
     };
 
-    function refresh() {
-        console.log("Called refresh");
-        $facebook.api("/me").then(
-            function(res) {
-                set_fb_credentials($facebook.getAuthResponse().accessToken);
-                $scope.isFacebookLoggedIn = true;
-            }
-        );
-    };
+    //$scope.login = function(){
+    //    $facebook.login().then(function(response) {
+    //        refresh();
+    //    });
+    //};
+
+    //function refresh() {
+    //    console.log("Called refresh");
+    //    $facebook.api("/me").then(
+    //        function(res) {
+    //            set_fb_credentials($facebook.getAuthResponse().accessToken);
+    //            $scope.isFacebookLoggedIn = true;
+    //        }
+    //    );
+    //};
 
     //refresh();
 
