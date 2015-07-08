d78c97cda73d4037e366830800524a3056dd717b
Ivan Hell <hell.ivan@yahoo.com>
2015-07-09 00:29:12 +0200

    Reorganized project and implemented new views
    - Splitted up project in public, admin, example
    - Implemented support api-keys (mongodb-backend)
    - added gitignore
    - Implemented simple administration GUI (admin)
    - Implemented simple registration GUI (public)
    - Reorganized server code so that there are now dataprovides for each api  (e.g twitter, facebook)
    - Outsourced config to mognodb
    - Checking api-keys using passport-localapikey

--- app.js
+++ app.js
@@ -1,242 +1,37 @@
-var app = angular.module('documee_demo', ['ui.bootstrap', 'ui.router']);
+var app = angular.module('documee_api_access', ['ui.bootstrap', 'ui.router']);
 
-app.controller("PostFbStatusController", function($http, $scope ){
+app.controller("mainController", function($http, $scope){
     var api_base_address = "http://localhost:8000/"
 
-    $scope.status = undefined;
 
-    $scope.postStatus = function(){
-        $http.post(api_base_address + "fb/status", {status: $scope.status}).
+    $scope.check_api_key = function(){
+        $http.get(api_base_address + "api/key/" + $scope.api_key +"/authorized").
             success(function(data, status, headers, config) {
                 console.log(data);
-                $scope.status = undefined;
             }).
             error(function(data, status, headers, config) {
                 console.log("Error: " + data);
             });
+
     };
+
+    $scope.request_api_key = function(){
+        var params = {
+            email : $scope.email,
+            company_name : $scope.company_name,
+            project_name : $scope.project_name,
+            project_description: $scope.project_description,
+            project_url : $scope.project_url
+        };
+
+        $http.get(api_base_address + "api/key", {params: params}).
+           success(function(data, status, headers, config) {
+               console.log(data);
+           }).
+           error(function(data, status, headers, config) {
+               console.log("Error: " + data);
+           });
+   };
 });
 
-app.controller("PostTwitterStatusController", function($http, $scope ){
-    var api_base_address = "http://localhost:8000/"
 
-    $scope.status = undefined;
-
-    $scope.postStatus = function(){
-        $http.post(api_base_address + "twitter/status", {status: $scope.status}).
-            success(function(data, status, headers, config) {
-                console.log(data);
-                $scope.status = undefined;
-            }).
-            error(function(data, status, headers, config) {
-                console.log("Error: " + data);
-            });
-    };
-});
-
-app.controller("mainController", function ($http, $scope, $rootScope) {
-    var api_base_address = "http://localhost:8000/"
-
-    OAuth.initialize("U7oog1cN5o_ZsjeoQ_rPOxbFaKA");
-
-    $scope.fbstate = {
-        states : [
-            {
-                name: 'get_permissions',
-                description: 'Fetch all app-permissions',
-                api : {
-                    method : 'get',
-                    call : api_base_address + 'fb/permissions'
-                },
-                template : 'partials/facebook/permissions-list.html'
-            },
-            {
-                name: 'delete_permissions',
-                description: 'Delete all Facebook app-permissions',
-                api : {
-                    method : 'delete',
-                    call : api_base_address + 'fb/permissions'
-                }
-            },
-            {
-                name: 'get_friends',
-                description: 'Fetch tagable friends from Facebook',
-                api : {
-                    method : 'get',
-                    call : api_base_address + 'fb/friends'
-                },
-                template : 'partials/facebook/friends-list.html'
-            },
-            {
-                name: 'get_me',
-                description: 'Fetch profile-infos from Facebook',
-                api : {
-                    method : 'get',
-                    call : api_base_address + 'fb/me'
-                },
-                template : 'partials/facebook/user-profile.html'
-            },
-            {
-                name: 'get_feeds',
-                description: 'Fetch latest feeds on Facebook',
-                api : {
-                    method : 'get',
-                    call : api_base_address + 'fb/feeds'
-                },
-                template : 'partials/facebook/feeds-list.html'
-            },
-            {
-                name : 'post_status',
-                description : 'Post status-update on facebook',
-                template : 'partials/facebook/post-update.html'
-            }
-
-        ],
-        current : undefined
-    };
-
-    $scope.twitterstate = {
-        states : [
-            {
-                name: 'get_friends',
-                description: 'Fetch friends on twitter',
-                api : {
-                    method : 'get',
-                    call : api_base_address + 'twitter/friends'
-                },
-                template : 'partials/twitter/friends-list.html'
-            },
-            {
-                name: 'delete_following',
-                description: 'Fetch people following on twitter',
-                api : {
-                    method : 'get',
-                    call : api_base_address + 'twitter/following'
-                },
-                template : 'partials/twitter/friends-list.html'
-            },
-            {
-                name: 'get_followers',
-                description: 'Fetch followers on twitter',
-                api : {
-                    method : 'get',
-                    call : api_base_address + 'twitter/followers'
-                },
-                template : 'partials/twitter/friends-list.html'
-            },
-            {
-                name: 'get_trends',
-                description: 'Fetch top 10 trends on twitter',
-                api : {
-                    method : 'get',
-                    call : api_base_address + 'twitter/trends'
-                },
-                template : 'partials/twitter/trends-list.html'
-            },
-            {
-                name : 'get_user',
-                description : 'Fetch user information on twitter',
-                api : {
-                    method : 'get',
-                    call : api_base_address + 'twitter/me'
-                },
-                template : 'partials/twitter/user-profile.html'
-            },
-            {
-                name : 'post_status',
-                description : 'Post status-update on twitter',
-                template : 'partials/twitter/post-update.html'
-            }
-        ],
-        current : undefined
-    };
-
-    $scope.switchFbState = function(state){
-        $scope.fb_result = undefined;
-        $scope.fbstate.current = undefined;
-
-        if(state){
-            if(state.api){
-                $http[state.api.method](state.api.call).
-                    success(function(data, status, headers, config) {
-                        console.log(data);
-                        $scope.data_fb = data;
-                        $scope.fbstate.current = state.name;
-                    }).
-                    error(function(data, status, headers, config) {
-                        console.log("Error: " );
-                        console.log(data);
-                    });
-            } else if(state.template) {
-                $scope.fbstate.current = state.name;
-            }
-        }
-    };
-
-    $scope.switchTwitterState = function(state){
-        $scope.twitter_result = undefined;
-        $scope.twitterstate.current = undefined;
-
-        if(state){
-            if(state.api){
-                $http[state.api.method](state.api.call).
-                    success(function(data, status, headers, config) {
-                        console.log(data);
-                        $scope.data_twitter = data;
-                        $scope.twitterstate.current = state.name;
-                    }).
-                    error(function(data, status, headers, config) {
-                        console.log("Error: " + data);
-                    });
-            } else if(state.template) {
-                $scope.twitterstate.current = state.name;
-            }
-        }
-    };
-
-    $scope.auth = {};
-
-    $scope.auth.fb ={
-        authenticated : false,
-        credentials : {}
-    };
-
-    $scope.auth.twitter ={
-        authenticated : false,
-        credentials : {}
-    };
-
-    function set_fb_credentials(access_token){
-        $scope.auth.fb.authenticated = true;
-        $scope.auth.fb.credentials.access_token = access_token;
-        $http.defaults.headers.common.fb_access_token = access_token;
-    }
-
-    function set_twitter_credentials(oauth_token, oauth_token_secret){
-        $scope.auth.twitter.authenticated = true;
-        $scope.auth.twitter.credentials.oauth_token = oauth_token;
-        $scope.auth.twitter.credentials.oauth_token_secret = oauth_token_secret;
-        $http.defaults.headers.common.twitter_oauth_token = oauth_token;
-        $http.defaults.headers.common.twitter_oauth_token_secret = oauth_token_secret;
-    }
-
-
-
-    $scope.loginTwitter = function(){
-        OAuth.popup('twitter', {cache: false}).done(function(result) {
-            console.log("Authenticated with twitter");
-            console.log(result);
-            set_twitter_credentials(result.oauth_token, result.oauth_token_secret);
-            $rootScope.$apply();
-        });
-    };
-
-    $scope.loginFacebook = function(){
-        OAuth.popup('facebook', {cache: false}).done(function(result) {
-            console.log("Authenticated with facebook");
-            console.log(result);
-            set_fb_credentials(result.access_token);
-            $rootScope.$apply();
-        });
-    };
-});
